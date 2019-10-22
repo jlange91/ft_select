a8f8102e641e5abcbc525c141695c816fa9d3fdc
Theophile DELMAS <thdelmas@e1r2p14.42.fr>
2019-10-22 20:19:42 +0200

    FIX signal

--- sources/main.c
+++ sources/main.c
@@ -6,24 +6,54 @@
 /*   By: thdelmas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 17:32:45 by thdelmas          #+#    #+#             */
-/*   Updated: 2019/10/22 18:11:16 by thdelmas         ###   ########.fr       */
+/*   Updated: 2019/10/22 20:18:26 by thdelmas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <termios.h>
+#include <term.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 
 #include "libft.h"
 #include "ft_select.h"
 
+struct 	termios reset;
+
+void destructor(void) __attribute__((destructor));
+
+void destructor(void)
+{
+	ft_putendl("Reset terminal");
+	tcsetattr(STDIN_FILENO, TCSADRAIN, &reset);
+}
+
+void	ft_handle_signals(int sig)
+{
+	ft_putnbr(sig);
+	ft_putchar('\n');
+	exit(1);
+}
+
+void	ft_set_signals()
+{
+	int i;
+
+	i = 0;
+	while (i <= 32)
+	{
+		signal(i, ft_handle_signals);
+		i++;
+	}
+}
+
 int	main()
 {
-	struct 	termios reset;
 	struct 	termios term;
 	int	nb_bytes;
 	char	*buff;
 	char	stop = 0;
+	char	*tc;
 
 	nb_bytes = 0;
 	tcgetattr(STDIN_FILENO, &reset);
@@ -32,6 +62,12 @@ int	main()
 	term.c_cc[VMIN] = 1;
 	term.c_cc[VTIME] = 0;
 	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	
+	ft_set_signals();
+
+	tgetent(NULL, "vt100");
+	tc = tgetstr("cl", NULL);
+	tputs(tc, 1, (int (*)(int))ft_putchar);
 	while (!stop)
 	{
 		ioctl(STDIN_FILENO, FIONREAD, &nb_bytes);
@@ -43,14 +79,15 @@ int	main()
 			stop = 1;
 		if (!ft_strcmp(ARROW_UP, buff))
 			ft_putstr(ARROW_UP);
-		if (!ft_strcmp(ARROW_DOWN, buff))
+		else if (!ft_strcmp(ARROW_DOWN, buff))
 			ft_putstr(ARROW_DOWN);
-		if (!ft_strcmp(ARROW_LEFT, buff))
+		else if (!ft_strcmp(ARROW_LEFT, buff))
 			ft_putstr(ARROW_LEFT);
-		if (!ft_strcmp(ARROW_RIGHT, buff))
+		else if (!ft_strcmp(ARROW_RIGHT, buff))
 			ft_putstr(ARROW_RIGHT);
+		else
+			ft_putstr(buff);
 		ft_strdel(&buff);
 	}
-	tcsetattr(STDIN_FILENO, TCSANOW, &reset);
 	return (0);
 }
