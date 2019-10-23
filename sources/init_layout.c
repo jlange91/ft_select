b977588dd19377c952b1b4147a77ac195cc42ce1
Theophile DELMAS <thdelmas@e1r7p19.42.fr>
2019-10-23 15:04:37 +0200

    EDT init layout nb_args

--- sources/init_layout.c
+++ sources/init_layout.c
@@ -6,7 +6,7 @@
 /*   By: thdelmas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 14:46:54 by thdelmas          #+#    #+#             */
-/*   Updated: 2019/10/23 14:56:25 by thdelmas         ###   ########.fr       */
+/*   Updated: 2019/10/23 15:04:26 by thdelmas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void	init_layout(int ac, char **av, t_layout *lay)
 	lay->col_len = 0;
 	lay->pos[0] = 0;
 	lay->pos[1] = 0;
-	if ((lay->params = init_params(ac, av)))
+	lay->nb_args = ac - 1;
+	if ((!lay->params = init_params(ac, av)))
 	{
 		ft_putendl_fd("ft_select: init_params: malloc_error", STDERR_FILENO);
 		exit(1);
